0ba5ab64d0d964cbb3071f9e28c58aafd888fc9e
Prachi24599 <prachi24599@gmail.com>
2023-10-22 15:52:36 +0530

    ItemDate omponent created

---
+++ src/components/ItemDate.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ItemDate.css";
+
+function ItemDate() {
+  return (
+    <div className="mfg-date">
+      <span>24</span>
+      <span>May</span>
+      <span>1999</span>
+    </div>
+  );
+}
+
+export default ItemDate;
