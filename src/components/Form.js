99730edc23b1e4711dea60ebce8e284e4d64011a
Prachi24599 <prachi24599@gmail.com>
2023-10-30 00:00:58 +0530

    using multiple useStates

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  function titleChangeHandler(event) {
+    setTitle(event.target.value);
+    console.log(event.target.value);
+  }
+  function dateChangeHandler(event) {
+    setDate(event.target.value);
+    console.log(event.target.value);
+  }
+
   return (
     <form>
       <div>
         <label>Title</label>
-        <input type="text" />
+        <input type="text" onChange={titleChangeHandler} />
       </div>
       <div>
         <label>Date</label>
-        <input type="date" min="2023-01-01" max="2023-12-12" />
+        <input
+          type="date"
+          onChange={dateChangeHandler}
+          min="2023-01-01"
+          max="2023-12-12"
+        />
       </div>
       <div>
         <button type="submit">Add Product</button>
