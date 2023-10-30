84a6732448e9a6c91ee92316c639981020de5716
Prachi24599 <prachi24599@gmail.com>
2023-10-31 00:00:03 +0530

    Card created for each city

---
+++ src/components/Card.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const Card = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
+  const description = `${info.substring(0, 200)}....`;
+
+  function readmoreHandler() {
+    setReadMore(!readMore);
+  }
+
+  function removeTourHandler() {}
+
+  return (
+    <div className="card">
+      <img src={image} alt="card-img" className="image" />
+
+      <div className="tour-info">
+        <div className="tour-details">
+          <h4 className="tour-price">{price}</h4>
+          <h4 className="tour-name">{name}</h4>
+        </div>
+
+        <div className="description">
+          {description}
+          <span className="read-mpre" onClick={readmoreHandler}>
+            {readMore ? `show less` : `read more`}
+          </span>
+        </div>
+      </div>
+
+      <button className="btn-red" onClick={removeTourHandler}>
+        Not Interested
+      </button>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Card from "./Card";
 
 const Tours = ({ tours }) => {
   return (
     <div>
       {tours.map(function (tour) {
-        return <Card></Card>;
+        return <Card {...tour} />;
       })}
     </div>
   );
