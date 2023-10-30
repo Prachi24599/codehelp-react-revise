5964d9ef6f281c19acc0491c5842dcd85c2a9767
Prachi24599 <prachi24599@gmail.com>
2023-10-30 21:02:57 +0530

    counter functionality added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function decrementCounter() {
+    setCount(count - 1);
+  }
+
+  function incrementCounter() {
+    setCount(count + 1);
+  }
+
+  function resetHandler() {
+    setCount(0);
+  }
   return (
     <div className="wrapper">
       <h3>Increment and Decrement</h3>
       <div className="container">
-        <button>-</button>
-        <div></div>
-        <button>+</button>
+        <button onClick={decrementCounter}>-</button>
+        <div>{count}</div>
+        <button onClick={incrementCounter}>+</button>
       </div>
-      <button>Reset</button>
+      <button onClick={resetHandler}>Reset</button>
     </div>
   );
 }
