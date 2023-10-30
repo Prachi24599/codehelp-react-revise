53cd65c16ab020c29d48cc5c894de06880d676dd
Prachi24599 <prachi24599@gmail.com>
2023-10-30 21:29:00 +0530

    Plan-With-Love Starter pack

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
-import { useState } from "react";
-import "./App.css";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  function decrementCounter() {
-    setCount(count - 1);
-  }
-
-  function incrementCounter() {
-    setCount(count + 1);
-  }
-
-  function resetHandler() {
-    setCount(0);
-  }
-  return (
-    <div className="wrapper">
-      <h3>Increment and Decrement</h3>
-      <div className="container">
-        <button onClick={decrementCounter}>-</button>
-        <div>{count}</div>
-        <button onClick={incrementCounter}>+</button>
-      </div>
-      <button onClick={resetHandler}>Reset</button>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
