b792150cd3e7790f575f5abf732edaf9947fdc90
Prachi24599 <prachi24599@gmail.com>
2023-10-30 20:55:10 +0530

    counter app created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello Ji</div>;
+  return (
+    <div className="wrapper">
+      <h3>Increment and Decrement</h3>
+      <div className="container">
+        <button>-</button>
+        <div></div>
+        <button>+</button>
+      </div>
+      <button>Reset</button>
+    </div>
+  );
 }
 
 export default App;
