486c15c337f5dfdb7d7b3b61fe4981474c8282b5
Prachi24599 <prachi24599@gmail.com>
2023-10-31 23:45:38 +0530

    Top Courses starter pack

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import React, { useState } from "react";
-import data from "./data.js";
-import Tours from "./components/Tours.js";
+import React from "react";
 
 const App = () => {
-  const [tours, setTours] = useState(data);
-
-  function removeTour(id) {
-    const newTours = tours.filter((tour) => tour.id !== id);
-    setTours(newTours);
-  }
-
-  if (!tours.length) {
-    return (
-      <div className="refresh">
-        <h2>No Tours Left</h2>
-        <button onClick={() => setTours(data)}>Refresh</button>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <h2>Plan With Love</h2>
-      <Tours tours={tours} removeTour={removeTour} />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
