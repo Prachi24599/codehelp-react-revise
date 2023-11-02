04eef7e47633e04e1850fe22dcaf74920e91f59f
Prachi24599 <prachi24599@gmail.com>
2023-11-03 00:13:36 +0530

    Top Couses Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [width, setWidth] = useState(window.innerWidth);
-  const [height, setHeight] = useState(window.innerHeight);
-
-  useEffect(() => {
-    function handleWindowResize() {
-      setWidth(window.innerWidth);
-      setHeight(window.innerHeight);
-    }
-
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  }, []);
-
   return (
     <div>
-      <h2>Width : {width}</h2>
-      <h2>Height : {height}</h2>
+      <h1>Hello World</h1>
     </div>
   );
 };
