9edc86ad190a644574ecd9fd398ad0e54d425e22
Prachi24599 <prachi24599@gmail.com>
2023-11-03 00:24:07 +0530

    export component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
-export const Filter = () => {
-  return (
-    <div>Filter</div>
-  )
-}
+const Filter = () => {
+  return <div>Filter</div>;
+};
+
+export default Filter;
