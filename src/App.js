035c4f9f2e59c8d26ab4c757eb8866efdc91d0c3
Prachi24599 <prachi24599@gmail.com>
2023-11-03 00:53:37 +0530

    fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Filter from "./components/Filter";
 import Cards from "./components/Cards";
 import { apiUrl, filterData } from "./data";
+import { toast } from "react-toastify";
 
 const App = () => {
+  const [courses, setCourses] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(apiUrl);
+        const output = await res.json();
+        console.log(output);
+        setCourses(output.data);
+      } catch (error) {
+        toast.error("Something went wrong");
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <Filter filterData={filterData} />
-      <Cards />
+      <Cards courses={courses} />
     </div>
   );
 };
