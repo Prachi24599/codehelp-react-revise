9e3d3c9bd2cda0deb3eae222d4206b2ea0669e6f
Prachi24599 <prachi24599@gmail.com>
2023-11-04 01:10:46 +0530

    css added for filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Filter = ({ filterData }) => {
   return (
-    <div>
+    <div className="w-11/12 flex flex-wrap max-w-max space-x-4 gap-y-4 mx-auto py-4 justify-center">
       {filterData.map((data) => (
         <button key={data.id}>{data.title}</button>
       ))}
