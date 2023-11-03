779c4ab518ee3ba7ec6d2b39d3c4aedda6e77e9f
Prachi24599 <prachi24599@gmail.com>
2023-11-04 00:26:45 +0530

    card component added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { FcLike } from "react-icons/fc";
 
 const Card = ({ course }) => {
   return (
     <div>
       <div>
         <img src={course.image.url} alt={course.image.alt} />
-        <div>
-          <button></button>
-        </div>
+        <button>
+          <FcLike />
+        </button>
+      </div>
+      <div>
+        <p>{course.title}</p>
+        <p>{course.description}</p>
       </div>
     </div>
   );
 };
 
 export default Card;
+//FcLikePlaceholder
