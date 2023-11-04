a55a70a2282e5b7001f1ac1e0018bb9bef20594a
Prachi24599 <prachi24599@gmail.com>
2023-11-05 00:07:12 +0530

    App structure created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Our Testimonials</h1>
+      <div></div>
+      <Testimonial />
+    </div>
+  );
 };
 
 export default App;
