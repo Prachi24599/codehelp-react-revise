42fccd95622ae43d49794d49e23733dab2af9e1b
Prachi24599 <prachi24599@gmail.com>
2023-11-05 00:03:37 +0530

    Testimonial slider starter

--- src/App.js
+++ src/App.js
@@ -1,47 +1,7 @@
-import React, { useEffect, useState } from "react";
-import Navbar from "./components/Navbar";
-import Filter from "./components/Filter";
-import Cards from "./components/Cards";
-import { apiUrl, filterData } from "./data";
-import { toast } from "react-toastify";
-import Spinner from "./components/Spinner";
+import React from "react";
 
 const App = () => {
-  const [courses, setCourses] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [category, setCategory] = useState(filterData[0].title);
-
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const res = await fetch(apiUrl);
-      const output = await res.json();
-      console.log(output.data);
-      setCourses(output.data);
-    } catch (error) {
-      toast.error("Something went wrong");
-      console.log(error);
-    }
-    setLoading(false); 
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <Filter
-        filterData={filterData}
-        category={category}
-        setCategory={setCategory}
-      />
-      <div className="w-11/12 max-w-[1200px] mx-auto  flex flex-wrap justify-center items-center min-h-[50vh]">
-        {loading ? <Spinner /> : <Cards courses={courses} category={category}/>}
-      </div>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
